a40562f0b725507a85f7bb7672119db2361366e0
Voodoo3000 <m047wbm@mail.ru>
2017-06-16 12:26:11 +0600

    Update HouseholdElectronic.java

--- oopFirstTask/src/com/studying/HouseholdElectronic.java
+++ oopFirstTask/src/com/studying/HouseholdElectronic.java
@@ -8,9 +8,6 @@ package com.studying;
  прибор в квартире, соответствующий заданному диапазону параметров.
  */
 public abstract class HouseholdElectronic {
-    int pcpower = 300;
-    int tvpower = 240;
-    int routerpower = 50;
     public void plugIn(int power) {
         System.out.println("Pluging in socket, consumed power is: " + power + "W");
     }
